1772e3465929678f3e5f81e7581417a4226bdfdd
Alphakas <alphakas@Alphakass-iMac.local>
2021-07-14 00:03:08 +0530

    test

--- src/views/page/GoPdf.vue
+++ src/views/page/GoPdf.vue
@@ -1,22 +1,12 @@
 <template>
   <div class="content g2-content">
-    <vue-pdf-app :pdf="url"></vue-pdf-app>
+    <vue-pdf-app pdf="url"></vue-pdf-app>
   </div>
 </template>
 
 <script>
 import { decode64 } from "@utils/AcrouUtil";
 import VuePdfApp from "vue-pdf-app";
-
-function base64ToArrayBuffer(base64) {
-  var binary_string = window.atob(base64);
-  var len = binary_string.length;
-  var bytes = new Uint8Array(len);
-  for (var i = 0; i < len; i++) {
-    bytes[i] = binary_string.charCodeAt(i);
-  }
-  return bytes.buffer;
-}
 
 export default {
   components: {
@@ -28,8 +18,9 @@ export default {
   computed: {
     url() {
       if (this.$route.params.path) {
-          return base64ToArrayBuffer(this.$route.params.path);
-        //return decode64(this.$route.params.path);
+          const decoded = decode64(this.$route.params.path);
+
+          return decoded;
       }
       return ''
     }
