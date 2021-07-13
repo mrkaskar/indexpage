93b90384ac44d06af111a72e95b51f051935513a
Alphakas <alphakas@Alphakass-iMac.local>
2021-07-13 23:27:07 +0530

    pdftest

--- src/views/page/GoPdf.vue
+++ src/views/page/GoPdf.vue
@@ -8,6 +8,16 @@
 import { decode64 } from "@utils/AcrouUtil";
 import VuePdfApp from "vue-pdf-app";
 
+function base64ToArrayBuffer(base64) {
+  var binary_string = window.atob(base64);
+  var len = binary_string.length;
+  var bytes = new Uint8Array(len);
+  for (var i = 0; i < len; i++) {
+    bytes[i] = binary_string.charCodeAt(i);
+  }
+  return bytes.buffer;
+}
+
 export default {
   components: {
     VuePdfApp
@@ -18,7 +28,8 @@ export default {
   computed: {
     url() {
       if (this.$route.params.path) {
-        return decode64(this.$route.params.path);
+          return base64ToArrayBuffer(this.$$route.params.path);
+        //return decode64(this.$route.params.path);
       }
       return ''
     }
